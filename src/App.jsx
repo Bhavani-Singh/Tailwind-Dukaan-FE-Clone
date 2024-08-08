64b37de29e9b602b2a7893f313fdf4dc8aa1c1e2
Bhavani-Singh <bhavanisinghrajput24@gmail.com>
2024-08-08 17:59:43 +0530

    add side panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import RightSideBar from './assets/components/LeftSideBar'
+import MainContent from './assets/components/MainContent'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1 className="bg-red-500">Hi There!</h1>
+    <div className="grid grid-cols-8">
+      <RightSideBar className="bg-[#1E2640] h-[100dvh] text-[#D2D4D9] flex flex-col justify-between"/>
+      <MainContent className="col-span-7 h-[100dvh]"/>
     </div>
   )
 }
